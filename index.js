6f9b7b3defc05a0b5660cb927cfba5a61f2e27ef
Touhid Tamim <touhidtamim1@gmail.com>
2025-05-24 23:19:42 +0600

    final commit before deployment

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("skillnest");
     const tasksCollection = db.collection("tasks");
 
